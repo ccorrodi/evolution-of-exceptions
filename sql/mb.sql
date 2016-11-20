04962b13aa0f47789c7c5ccd5f039255caa5a7aa
Jakob Schaerer <jakob.schaerer@students.unibe.ch>
2016-11-20 22:26:36 +0100

    added results

--- sql/mb.sql
+++ sql/mb.sql
@@ -1,3 +1,5 @@
+
+-- minus md and mu
 SELECT count(DISTINCT source), commit_timestamp FROM method_throws_declarations t1
 	JOIN method_throws_declaration_types t2 ON t1.id = t2.method_throws_declaration_id
   JOIN commits t3 ON t3.id = t1.commit_id
--- sql/mu.sql
+++ sql/mu.sql
@@ -1,5 +1,5 @@
 -- change the project_id
-SELECT count(DISTINCT source), commit_timestamp, userdefined FROM method_throws_declarations t1
+SELECT count(DISTINCT source), commit_timestamp FROM method_throws_declarations t1
 	JOIN method_throws_declaration_types t2 ON t1.id = t2.method_throws_declaration_id
   JOIN commits t3 ON t3.id = t1.commit_id
   WHERE project_id = 1 AND userdefined = true
